976b1faec9ca9bf95848e438c9226cda73cc5652
Masaharu TASHIRO <masatsr.kit@gmail.com>
2019-05-24 23:35:04 +0900

    [Add] メイン処理をgo routineして無限ループで待つプログラム

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	osSignalCh := make(chan os.Signal, 1)
+	signal.Notify(osSignalCh, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer signal.Stop(osSignalCh)
+
+	done := make(chan bool)
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Printf("Hello, %dth World!\n", i)
+			time.Sleep(time.Second * 1)
+		}
+		done <- true
+	}()
+
+	for {
+		select {
+		case killSignal := <-osSignalCh:
+			switch killSignal {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				log.Println("killed")
+				time.Sleep(5 * time.Second)
+				return
+			}
+		case <-done:
+			log.Println("done")
+			return
+		}
+	}
 }
